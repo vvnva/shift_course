52c9d9bafe39059b8c52fb50ad4b26f60fe9e69d
vvnva <vvivanova_10@edu.hse.ru>
2024-04-18 02:55:03 +0300

    hw10 - linter, tests - исправления 2

--- tests/app/core/calculator/calculator_test.py
+++ tests/app/core/calculator/calculator_test.py
@@ -16,8 +16,8 @@ class TestNewCalculator:
         'proba, avg_income_per_adult, age, interest_rate, share_for_loan, weighted_ext, expected_amount',
         [
             (0.05, 10000, 30, 0.05, 0.2, 0.6, 425000),  # Минимальная вероятность дефолта, отсальные переменные ниже порога
-            (0.1, 200000, 60, 0.08, 0.3, 0.7, 250000),  # Age category other amount
-            (0.2, 150000, 56, 0.08, 0.3, 0.7, 250000),  # Age category other amount
+            (0.1, 200000, 60, 0.08, 0.3, 0.7, 550000),  # Age category other amount ###
+            (0.2, 150000, 56, 0.08, 0.3, 0.7, 550000),  # Age category other amount ###
             (0.05, 150000, 30, 0.08, 0.3, 0.7, 825000),
             (0.05, 10000, 30, 0.08, 0.3, 0.7, 525000),
             (0.05, 10000, 30, 0.05, 0.5, 0.7, 500000),
