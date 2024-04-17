1d5d8e2b39a2a4788032cc2d21a1b3a7246d9eed
vvnva <vvivanova_10@edu.hse.ru>
2024-04-17 20:11:01 +0300

    hw9

---
+++ src/app/core/api.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+
+
+class ScoringDecision(Enum):
+    """Возможные решения модели."""
+
+    ACCEPTED = auto()
+    DECLINED = auto()
+
+@dataclass
+class ScoringResult:
+    """Класс, содержащий результаты скоринга."""
+
+    decision: ScoringDecision
+    amount: int
+    threshold: float
+    proba: float
+
+@dataclass
+class Features:
+    """Фичи для принятия решения об одобрении."""
+    interest_rate: float = 0
+    share_for_loan: float = 0
+    weighted_ext: float = 0
+    age: float = 0
+    avg_income_per_adult: float = 0
+    doc_change_years_ago: float = 0
+    diff_days: float = 0
+    application_credit_ratio: float = 0
+    credit_goods_ratio: float = 0
+    avg_children_per_adult: float = 0
+    complete_home_info: float = 0
+    doc_change_delay: float = 0
+    amt_credit_limit_actual_full_mean: float = 0
+    num_documents: float = 0
+    share_active_debt: float = 0
---
+++ src/app/core/calculator.py
@@ -0,0 +1,39 @@
+from src.app.core.api import Features
+
+
+class Calculator:
+    """Класс для расчета одобренной суммы."""
+
+    proba_threshold = 0.1
+    income_threshold = 140000
+
+    age_category_other_amount = 150000
+    default_amount = 25000
+
+    interest_rate_threshold = 0.07
+    share_for_loan_threshold = 0.23
+    weighted_ext_threshold = 0.65
+
+    def calc_amount(
+            self,
+            proba: float,
+            features: Features,
+    ) -> int:
+        """Функция принимает на вход вероятность дефолта и признаки и расчитывает одобренную сумму."""
+        #варирование по age, interest_rate_threshold, weighted_ext_threshold скору, share for loan, avg_income_per_adult
+
+        amount = self.default_amount
+        if proba < self.proba_threshold:
+            amount += 400000
+        if features.avg_income_per_adult > self.income_threshold:
+            amount += 300000
+        if features.age > 55:
+            amount = self.age_category_other_amount
+        if features.interest_rate > self.interest_rate_threshold:
+            amount += 25000
+        if features.share_for_loan > self.share_for_loan_threshold:
+            amount += 35000
+        if features.weighted_ext > self.weighted_ext_threshold:
+            amount += 40000
+
+        return amount
